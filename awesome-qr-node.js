3b66e71a728dff78aa93ea4d9de0aa6a17da6c62
Amit Prabhu <amit.dp180@gmail.com>
2019-03-19 15:18:06 +0530

    Fix canvas import

--- awesome-qr-node.js
+++ awesome-qr-node.js
@@ -30,7 +30,7 @@
  * @see <a href="http://www.d-project.com/" target="_blank">http://www.d-project.com/</a>
  * @see <a href="http://jeromeetienne.github.com/jquery-qrcode/" target="_blank">http://jeromeetienne.github.com/jquery-qrcode/</a>
  */
-let Canvas = require('canvas-prebuilt');
+let Canvas = require('canvas');
 let Image = Canvas.Image;
 
 var AwesomeQRCode;
