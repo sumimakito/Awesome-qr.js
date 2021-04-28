aa105ff8c1145e8da24de9b50e4705adb6654e46
hritik <hritik.aggarwal31@gmail.com>
2021-04-28 13:10:01 +0530

    merged

--- src/tests/Index.test.ts
+++ src/tests/Index.test.ts
@@ -53,38 +53,6 @@ const config = {
    useOpacity: true,
    viewportSize: 891,
 };
-const config2 = {
-    backgroundColor:'white',
-    canvasType: CanvasType.PNG,
-    colorDark: "red",
-    colorLight: "green",
-    correctLevel: 2,
-    dataPattern: DataPattern.SQUARE,
-    dotScale: 0.96,
-    eyeBallShape: EyeBallShape.CIRCLE,
-    // eyeFrameColor: "",
-    eyeFrameShape: EyeFrameShape.SQUARE,
-    frameStyle: QRCodeFrame.CIRCULAR,
-    frameColor: "blue",
-    frameText: "",
-    gradientType: GradientType.RADIAL,
-   // imageServerURL: "https://beaconstacqa.mobstac.com/api/2.0/validate_url/",
-    isVCard: false,
-    logoCornerRadius: 8,
-    logoMargin: 11,
-    logoScale: 0.2,
-    margin: 80,
-    maskedDots: false,
-   moduleSize: 27,
-   nSize: 27,
-   rawSize: 1051,
-   size: 1051,
-    text: "https://qr.getwifireapp.com/zy9t9e",
-   typeNumber: 4,
-   useCanvas: false,
-   useOpacity: true,
-   viewportSize: 891,
-};
 
 describe('QR code tests', () => {
     it('Main test SVG', done => {
@@ -99,37 +67,6 @@ describe('QR code tests', () => {
             done();
         }).catch(err => {
            console.error(err);
-            done();
-        });
-    });
-    it('Main test PNG', done => {
-        const qrCodeGenerator = new QRCodeBuilder(config2);
-
-        qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
-            
-            const fs = require('fs');
-             
-            const dataUrl = qrCode.canvas.toDataURL('image/png');
-            const matches: any = dataUrl.match(
-                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
-                  ),
-                response: any  ={};
-            response.type = matches[1];
-            response.data = Buffer.from(matches[2], "base64");
-            const decodedImg = response;
-            const imageBuffer = decodedImg.data;
-            const extension ='png';
-            const fileName = '/test2' + "." + extension;
-            fs.writeFileSync(__dirname+fileName, imageBuffer);
-            // console.log(dataUrl.substr(0,200));
-            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
-            done();
-            
-            
-            // const fs = require('fs');
-            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
-            // done();
-        }).catch(err => {
             done();
         });
     });
