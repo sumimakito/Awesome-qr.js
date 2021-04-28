19cc68f9f3fb5be2bf1289f474996aaee4fbf9f1
hritik <hritik.aggarwal31@gmail.com>
2021-04-28 16:05:12 +0530

    increased coverage

--- src/tests/Index.test.ts
+++ src/tests/Index.test.ts
@@ -23,13 +23,13 @@ const config = {
     backgroundImage: "https://i.pinimg.com/474x/d4/48/2b/d4482ba4e7ebdbff7b8ba73e7d39aceb.jpg",
     logoImage: "https://i.pinimg.com/474x/d4/48/2b/d4482ba4e7ebdbff7b8ba73e7d39aceb.jpg",
     backgroundColor:'white',
-    canvasType: CanvasType.PNG,
+    canvasType: CanvasType.SVG,
     colorDark: "red",
     colorLight: "green",
     correctLevel: 2,
     dataPattern: DataPattern.SQUARE,
     dotScale: 0.96,
-    eyeBallShape: EyeBallShape.CIRCLE,
+    eyeBallShape: EyeBallShape.SQUARE,
     // eyeFrameColor: "",
     eyeFrameShape: EyeFrameShape.SQUARE,
     frameStyle: QRCodeFrame.BALLOON_BOTTOM,
@@ -69,7 +69,39 @@ const config2 = {
     frameText: "",
     gradientType: GradientType.RADIAL,
    // imageServerURL: "https://beaconstacqa.mobstac.com/api/2.0/validate_url/",
-    isVCard: false,
+    isVCard: true,
+    logoCornerRadius: 8,
+    logoMargin: 11,
+    logoScale: 0.2,
+    margin: 80,
+    maskedDots: false,
+   moduleSize: 27,
+   nSize: 27,
+   rawSize: 1051,
+   size: 1051,
+    text: "https://qr.getwifireapp.com/zy9t9e",
+   typeNumber: 4,
+   useCanvas: false,
+   useOpacity: true,
+   viewportSize: 891,
+};
+const config3 = {
+    backgroundColor:'white',
+    canvasType: CanvasType.PNG,
+    colorDark: "red",
+    colorLight: "green",
+    correctLevel: 2,
+    dataPattern: DataPattern.CIRCLE,
+    dotScale: 0.96,
+    eyeBallShape: EyeBallShape.LEFT_LEAF,
+    // eyeFrameColor: "",
+    eyeFrameShape: EyeFrameShape.RIGHT_LEAF,
+    frameStyle: QRCodeFrame.BANNER_BOTTOM,
+    frameColor: "blue",
+    frameText: "",
+    gradientType: GradientType.VERTICAL,
+   // imageServerURL: "https://beaconstacqa.mobstac.com/api/2.0/validate_url/",
+    isVCard: true,
     logoCornerRadius: 8,
     logoMargin: 11,
     logoScale: 0.2,
@@ -86,16 +118,69 @@ const config2 = {
    viewportSize: 891,
 };
 
+const config4 = {
+    backgroundColor:'white',
+    canvasType: CanvasType.PNG,
+    colorDark: "red",
+    colorLight: "green",
+    correctLevel: 2,
+    dataPattern: DataPattern.KITE,
+    dotScale: 0.96,
+    eyeBallShape: EyeBallShape.RIGHT_DIAMOND,
+    // eyeFrameColor: "",
+    eyeFrameShape: EyeFrameShape.LEFT_LEAF,
+    frameStyle: QRCodeFrame.BOX_TOP,
+    frameColor: "blue",
+    frameText: "",
+    gradientType: GradientType.HORIZONTAL,
+   // imageServerURL: "https://beaconstacqa.mobstac.com/api/2.0/validate_url/",
+    isVCard: true,
+    logoCornerRadius: 8,
+    logoMargin: 11,
+    logoScale: 0.2,
+    margin: 80,
+    maskedDots: true,
+   moduleSize: 27,
+   nSize: 27,
+   rawSize: 1051,
+   size: 1051,
+    text: "https://qr.getwifireapp.com/zy9t9e",
+   typeNumber: 4,
+   useCanvas: true,
+   useOpacity: true,
+   viewportSize: 891,
+};
 describe('QR code tests', () => {
-    it('Main test SVG', done => {
+    it('Main test SVG 1', done => {
         const qrCodeGenerator = new QRCodeBuilder(config);
 
         qrCodeGenerator.build(CanvasType.SVG).then(qrCode => {
             
-
-
             const fs = require('fs');
             fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+        }).catch(err => {
+           console.error(err);
+            done();
+        });
+    });
+    it('Main test SVG 2', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config3);
+        qrCodeGenerator.build(CanvasType.SVG).then(qrCode => {
+            const fs = require('fs');
+            fs.writeFileSync(__dirname + '/test3.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+        }).catch(err => {
+           console.error(err);
+            done();
+        });
+    });
+    it('Main test SVG 3', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config4);
+
+        qrCodeGenerator.build(CanvasType.SVG).then(qrCode => {
+            const fs = require('fs');
+            fs.writeFileSync(__dirname + '/test4.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
             done();
         }).catch(err => {
            console.error(err);
